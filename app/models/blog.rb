ac27c4fa515e6c6442e7b283f8e30c376dbdec43
Andy Strube <andystrube64@gmail.com>
2020-02-02 20:20:39 -0800

    Added time since creation information to the subblog and user show pages

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,4 +5,39 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1 }
   validates :content, presence: true, length: { minimum: 1 }
 
+  def time_since_post
+    seconds = (Time.now - self.created_at).to_i
+    minutes = (seconds / 60).to_i
+    hours = (minutes / 60).to_i
+    days = (hours / 24).to_i
+    weeks = (days / 7).to_i
+    months = (weeks / 4.333).to_i
+    years = (weeks / 52).to_i
+    message = ""
+    if seconds < 60
+      message = "less than a minute ago "
+    elsif minutes < 2
+      message = "a minute ago "
+    elsif minutes < 60
+      message = "#{minutes} minutes ago "
+    elsif hours < 2
+      message = "an hour ago "
+    elsif hours < 24
+      message = "#{hours} hours ago "
+    elsif days < 2
+      message = "a day ago "
+    elsif days < 32
+      message = "#{days} days ago "
+    elsif months < 2
+      message = "a month ago "
+    elsif months < 12
+      message = "#{months} months ago "
+    elsif years < 2
+      message = "a year ago "
+    else
+      message = "#{years} years ago "
+    end
+    message
+  end
+
 end
