80966dcd707cc7076f61cfe64eded260d7660700
Andy Strube <andystrube64@gmail.com>
2020-02-06 22:13:56 -0800

    Minor adjustement for a test

--- spec/controllers/moderator/blogs_controller_spec.rb
+++ spec/controllers/moderator/blogs_controller_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe Moderator::BlogsController, type: :controller do
   describe "blogs#show action" do
     it "should successfully show the page" do
       subblog = FactoryBot.create(:subblog)
+      sign_in subblog.user
       blog = FactoryBot.create(:blog)
       get :show, params: { id: blog.id, subblog_id: subblog.id }
-      expect(response).to have_http_status(:found)
+      expect(response).to have_http_status(:success)
     end
   end
 
