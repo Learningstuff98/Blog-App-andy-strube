0125ef862d12f915301fc7c084167a827f70ef62
Andy Strube <andystrube64@gmail.com>
2020-02-11 16:43:20 -0800

    Users can delete comments that they made

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,23 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!, only: [:create, :destroy]
   
   def create
     @subblog = Subblog.find(params[:subblog_id])
     @blog = Blog.find(params[:blog_id])
     @comment = @blog.comments.create(comment_params.merge(user: current_user))
     redirect_to subblog_blog_path(@subblog, @blog)
+  end
+
+  def destroy
+    @subblog = Subblog.find(params[:subblog_id])
+    @blog = Blog.find(params[:blog_id])
+    @comment = Comment.find(params[:id])
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to subblog_blog_path(@subblog, @blog)
+    else
+      render plain: 'Unauthorized', status: :unauthorized
+    end
   end
 
   private
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -22,4 +22,35 @@ RSpec.describe CommentsController, type: :controller do
     end
   end
 
+  describe "comments@destroy action" do
+    it "should allow the person who posted the comment to delete it" do
+      subblog = FactoryBot.create(:subblog)
+      blog = FactoryBot.create(:blog)
+      comment = FactoryBot.create(:comment)
+      sign_in comment.user
+      delete :destroy, params: { id: comment.id, subblog_id: subblog.id, blog_id: blog.id }
+      expect(response).to have_http_status(:found)
+      comment = Comment.find_by_id(comment.id)
+      expect(comment).to eq nil
+    end
+
+    it "should require that a user be logged in" do
+      subblog = FactoryBot.create(:subblog)
+      blog = FactoryBot.create(:blog)
+      comment = FactoryBot.create(:comment)
+      delete :destroy, params: { id: comment.id, subblog_id: subblog.id, blog_id: blog.id }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+    it "should only allow the user who posted the comment to delete it" do
+      subblog = FactoryBot.create(:subblog)
+      blog = FactoryBot.create(:blog)
+      comment = FactoryBot.create(:comment)
+      user = FactoryBot.create(:user)
+      sign_in user
+      delete :destroy, params: { id: comment.id, subblog_id: subblog.id, blog_id: blog.id }
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
 end
